be7c9fe0f7cd86a801bbe4992b95eb38841ac1a5
lamont <lamont@fastrobot.com>
2022-05-04 08:58:30 -0700

    working end to end prom scraper -> amp -> managed grafana

--- monitoring/README.md
+++ monitoring/README.md
@@ -1,24 +1,29 @@
 # FR approved Monitoring stack
 Sets up:
 * AWS Managed Prometheus backend (done)
-* ECS cluster
-  * Prometheus task
-    * prometheus server
-    * prometheus-sdconfig-reloader
+* ECS cluster (done)
+  * Prometheus scraper task (done)
+    * prometheus server (done)
+    * prometheus-sdconfig-reloader (done)
   * node_exporter (daemonset)
   * blackbox
   * cadvisor (daemonset)
   * grafana (optional!)
 * Grafana
   * in ecs, cheapest?
-  * via aws managed (default)
+  * via aws managed (default) (done)
   * via grafana cloud (fanciest)
+
+## AMP Metric Retention
+from https://docs.aws.amazon.com/prometheus/latest/userguide/what-is-Amazon-Managed-Service-Prometheus.html
+> Metrics ingested into a workspace are stored for 150 days, and are then automatically deleted.
 
 ## AMP Ruler and AlertManager
 * use https://docs.aws.amazon.com/prometheus/latest/userguide/AMP-Ruler.html to pass a map of names of yaml strings in by namespace
 * Expects prometheus standard rule and alert configuration 
 * express the inputs however you like. Examples include templates through terragrunt or direct yaml conversion of hcl types in the calling module
 
+No modules.
 
 Roughly based off of the structure from https://github.com/aws-samples/prometheus-for-ecs
 
--- monitoring/main.tf
+++ monitoring/main.tf
@@ -1,7 +1,6 @@
 locals {
   create_amp        = alltrue([var.enable, var.enable_amp])
   create_prometheus = alltrue([var.enable, var.enable_prometheus])
-  create_grafana    = alltrue([var.enable, var.enable_grafana_managed])
   full_name         = "${var.namespace}-${var.name}-${var.environment}"
   tags = {
     Environment = var.environment
@@ -29,4 +28,185 @@ resource "aws_prometheus_alert_manager_definition" "prom-alerts" {
   for_each     = var.alerts
   definition   = each.value
   workspace_id = aws_prometheus_workspace.prom[0].id
+}
+
+
+resource "aws_efs_file_system" "prom_service_storage" {
+  encrypted = true
+  tags = {
+    Name = "${local.full_name}-prometheus-config"
+  }
+}
+
+resource "aws_efs_mount_target" "prom_service_storage" {
+  count = length(var.private_subnet_ids)
+
+  file_system_id  = aws_efs_file_system.prom_service_storage.id
+  subnet_id       = var.private_subnet_ids[count.index]
+  security_groups = [aws_security_group.prom_efs_sg.id]
+}
+
+# define the prometheus config-reloader (from https://github.com/aws-samples/prometheus-for-ecs)
+# This is the least hacky way I can find to get a templated prometheus.yml into the stock prometheus
+# container task. The 4 variables in the environment control the frequency at which this sidecar task
+# will check ParameterStore for a prometheus.yml
+# this container will template configs onto a EFS mount shared with a prometheus server
+module "reloader_container_def" {
+  count = local.create_prometheus ? 1 : 0
+
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
+
+  container_name   = "${local.full_name}-prometheus-config-reloader"
+  container_image  = "public.ecr.aws/awsvijisarathy/prometheus-sdconfig-reloader:4.0"
+  container_memory = 128 # tiny  process, tiny memory
+  container_cpu    = 10
+  user = "root"
+  map_environment = {
+    CONFIG_FILE_DIR                     = "/etc/config"
+    CONFIG_RELOAD_FREQUENCY             = 60
+    PROMETHEUS_CONFIG_PARAMETER_NAME    = "/${var.environment}/ECS-Prometheus-Configuration"
+    DISCOVERY_NAMESPACES_PARAMETER_NAME = "/${var.environment}/ECS-ServiceDiscovery-Namespaces"
+    AWS_REGION                          = data.aws_region.current.name
+  }
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = "/ecs/Prometheus"
+      awslogs-create-group  = true
+      awslogs-region        = data.aws_region.current.name
+      awslogs-stream-prefix = "${local.full_name}-reloader"
+    }
+  }
+  mount_points = [
+    {
+      containerPath = "/etc/config"
+      sourceVolume  = "${local.full_name}-prometheus-config"
+      readOnly      = false
+    }
+  ]
+}
+
+resource "aws_ssm_parameter" "prom_config" {
+  name  = "/${var.environment}/ECS-Prometheus-Configuration"
+  type  = "String"
+  value = replace(var.prometheus_config, "REMOTE_WRITE_URL", "${aws_prometheus_workspace.prom[0].prometheus_endpoint}api/v1/remote_write")
+}
+
+resource "aws_ssm_parameter" "prom_sd_ns" {
+  name  = "/${var.environment}/ECS-ServiceDiscovery-Namespaces"
+  type  = "String"
+  value = "ecs-services"
+}
+
+# define the prometheus scraper container
+# this container will scrape various prometheus exporters, then remote_write all data to the AMP prometheus endpoint
+module "prometheus_container_def" {
+  count = local.create_prometheus ? 1 : 0
+
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
+
+  container_name   = "${local.full_name}-prometheus"
+  container_image  = var.prometheus_image
+  container_memory = var.prometheus_container_memory
+  container_cpu    = var.prometheus_container_cpu
+  user = "root"
+  port_mappings = [
+    {
+      containerPort = 9090
+      hostPort      = 9090 # ?maybe?
+      protocol      = "tcp"
+    }
+  ]
+  command = [
+    "--storage.tsdb.retention.time=15d",
+    "--config.file=/etc/config/prometheus.yaml",
+    "--storage.tsdb.path=/data",
+    "--web.console.libraries=/etc/prometheus/console_libraries",
+    "--web.console.templates=/etc/prometheus/consoles",
+    "--web.enable-lifecycle"
+  ]
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = "/ecs/Prometheus"
+      awslogs-create-group  = true
+      awslogs-region        = data.aws_region.current.name
+      awslogs-stream-prefix = "${local.full_name}-prometheus"
+    }
+  }
+  mount_points = [
+    {
+      containerPath = "/etc/config"
+      sourceVolume  = "${local.full_name}-prometheus-config"
+      readOnly      = true
+    },
+    {
+      containerPath = "/data"
+      sourceVolume  = "${local.full_name}-prometheus-data"
+      readOnly      = false
+    }
+  ]
+  healthcheck = {
+    command = [
+      "CMD-SHELL",
+      "wget http://localhost:9090/-/healthy -O /dev/null || exit 1"
+    ]
+    retries     = 2
+    timeout     = 2
+    interval    = 10
+    startPeriod = 10
+  }
+  container_depends_on = [
+    {
+      containerName = "${local.full_name}-prometheus-config-reloader"
+      condition     = "START"
+    }
+  ]
+}
+
+# join all above containers into a single task
+resource "aws_ecs_task_definition" "prom_stack" {
+  count = local.create_prometheus ? 1 : 0
+  container_definitions = jsonencode([
+    module.reloader_container_def[0].json_map_object,
+    module.prometheus_container_def[0].json_map_object
+  ])
+  cpu                      = 256
+  memory                   = 512
+  family                   = "${local.full_name}-prometheus-stack"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  task_role_arn            = aws_iam_role.prom-scraper-app-role.arn            # perms for the actual prom scraper
+  execution_role_arn       = aws_iam_role.prom-scraper-task-execution-role.arn # perms for the ecs agent to launch the containers
+  volume {
+    name = "${local.full_name}-prometheus-config"
+    efs_volume_configuration {
+      file_system_id          = aws_efs_file_system.prom_service_storage.id
+      root_directory          = "/"
+      transit_encryption      = "ENABLED"
+      transit_encryption_port = 2049
+    }
+  }
+  volume {
+    # without any other identifiers, this becomes an ephemeral volume, which is fine cause we're remote-writing data to AMP
+    name = "${local.full_name}-prometheus-data"
+  }
+}
+
+resource "aws_ecs_service" "prom" {
+  name                               = "${local.full_name}-prometheus-scraper"
+  count                              = local.create_prometheus ? 1 : 0
+  cluster                            = module.ecs[0].ecs_cluster_id
+  task_definition                    = aws_ecs_task_definition.prom_stack[0].arn
+  desired_count                      = 1
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
+  launch_type                        = "FARGATE"
+  network_configuration {
+    subnets          = var.private_subnet_ids
+    security_groups  = [aws_security_group.prom_ecs_sg[0].id]
+    assign_public_ip = false
+  }
 }
